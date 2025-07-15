501751e8ae4c867172af8481583110e8e450acc8
JustNobleMe <isaacodey01@gmail.com>
2025-07-15 16:06:37 +0100

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ export default function Contact () {
         <section className='section--contact' id='contact'>
             <div className='section--container'>
                 <h1 className='contact--heading'>CONTACT</h1>
-                <p className='contact--info'>Nulla in velit a metus rhoncus tempus. Nulla congue nulla vel sem varius finibus. Sed ornare sit amet lorem sed viverra. In vel urna quis libero viverra facilisis ut ac est.</p>
+                <p className='contact--info'>Reach out via form, social media, or support platforms</p>
                 <img src='./images/separatorBlack1.png' alt='seperator' />
 
                 <form>
